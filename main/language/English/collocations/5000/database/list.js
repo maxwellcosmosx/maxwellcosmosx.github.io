859ed18fd5204acfe3b3dcc87ee56623fe506a32
maxwell <maxwellcosmosx@gmail.com>
2025-05-01 23:11:05 +0800

    updae

--- main/language/English/collocations/5000/database/list.js
+++ main/language/English/collocations/5000/database/list.js
@@ -1,7 +1,2 @@
 list = [
-[1199,"","","",""],
 ]
-[1181,"I'll say that.","NULL","","","0"],
-[1199,"Not that we can see.","NULL","","目前来看不是这样的","0"],
-[1199,"I think we are getting there","","",""],
-[1199,"you get me","","","你懂我"],

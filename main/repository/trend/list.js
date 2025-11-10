320adeed4bdf036d3af48b38b0f84e02a9606d3c
maxwell <maxwellcosmosx@gmail.com>
2025-11-10 19:47:45 +0800

    update

---
+++ main/repository/trend/list.js
@@ -0,0 +1,14 @@
+scoreData = [
+	{date: "25-11-11", score: 50},
+	{date: "25-11-12", score: 56},
+	{date: "25-11-13", score: 56},
+	{date: "25-11-14", score: 50},
+	{date: "25-11-15", score: 56},
+	{date: "25-11-16", score: 60},
+	{date: "25-11-17", score: 50},
+	{date: "25-11-18", score: 56},
+	{date: "25-11-19", score: 56},
+	{date: "25-11-20", score: 50},
+	{date: "25-11-21", score: 56},
+	{date: "25-11-22", score: 60},
+];
